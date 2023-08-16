e0d49ddd9a450d008919217b6eed9cd37418535d
Oumar Kouassi <okouassi@ebenyx.com>
2023-08-16 20:04:37 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { EntrepotComponent } from './entrepot/entrepot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    EntrepotComponent
   ],
   imports: [
     BrowserModule
